017b4163c86d20f1a2c765473e818e49f0f610fd
Miran Muthanna <43168423+miran93@users.noreply.github.com>
2023-01-02 17:34:43 +0530

    changed provider.tf

--- providers.tf
+++ providers.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 0.13.1"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.20.0"
+    }
+  }
+}
+
 provider "aws" {
     region = "${var.region}"
 }
